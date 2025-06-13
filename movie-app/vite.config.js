9b5e3c442852c93f03b94e2beb0bc9cdf6fd62eb
Sophie Choi <103692454+destructivebasket@users.noreply.github.com>
2025-06-13 12:47:17 -0700

    commit

--- movie-app/vite.config.js
+++ movie-app/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: '/movie-app-remastered/',
+  base: '/movie-app/',
 })
